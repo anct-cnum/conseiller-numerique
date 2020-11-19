412f789469b43295331f08f705d086467e5bae45
Thomas Recouvreux <thomas.recouvreux@nanxia.fr>
2020-11-19 20:49:18 +0100

    Update uat url

--- front/src/environments/environment.uat.ts
+++ front/src/environments/environment.uat.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://uat-app2.conseillernumerique.fr',
+  apiUrl: 'https://uat-cnum-back.osc-fr1.scalingo.io',
 
   reCaptachaSiteKey: '6LcmhuMZAAAAAKZLviqAL2AXqbpZve-PM18uyKe9',
 };
