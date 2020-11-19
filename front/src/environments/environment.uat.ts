753538300c23460af36117ad6db8f890f0eb572d
Thomas Recouvreux <thomas.recouvreux@nanxia.fr>
2020-11-19 20:59:14 +0100

    Update captcha key on uat

--- front/src/environments/environment.uat.ts
+++ front/src/environments/environment.uat.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://uat-cnum-back.osc-fr1.scalingo.io',
 
-  reCaptachaSiteKey: '6LcmhuMZAAAAAKZLviqAL2AXqbpZve-PM18uyKe9',
+  reCaptachaSiteKey: '6LczlOMZAAAAAP3h0xt-XlfSlqVQczT_nCxPV-dS',
 };
