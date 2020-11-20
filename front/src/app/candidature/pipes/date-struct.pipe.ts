2bbd245d4417924ed9f0d95061184f3a44570424
Thomas Recouvreux <thomas.recouvreux@nanxia.fr>
2020-11-20 15:59:59 +0100

    Fix 'other' date in prettydate form field

--- front/src/app/candidature/pipes/date-struct.pipe.ts
+++ front/src/app/candidature/pipes/date-struct.pipe.ts
@@ -1,7 +1,6 @@
 import {Pipe, PipeTransform} from '@angular/core';
 import {DatePipe} from '@angular/common';
-import {NgbDate} from '@ng-bootstrap/ng-bootstrap';
-import {date2api} from '../../core/services/api/utils';
+import {date2api, isDateStruct} from 'app/core/services/api/utils';
 
 
 @Pipe({
@@ -11,7 +10,7 @@ import {date2api} from '../../core/services/api/utils';
 export class DateStructPipe extends DatePipe implements PipeTransform {
 
   transform(value: any, format = 'mediumDate', timezone?: string, locale?: string): string|null {
-    if (value instanceof NgbDate) {
+    if (isDateStruct(value)) {
       value = date2api(value);
     }
     return super.transform(value, format, timezone, locale);
