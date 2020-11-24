8b678b44c8be00046d2ee6900599af315e08a300
Thomas Recouvreux <thomas.recouvreux@nanxia.fr>
2020-11-24 17:49:37 +0100

    Add admin action to run matching

--- back/djapp/admin.py
+++ back/djapp/admin.py
@@ -1,6 +1,12 @@
-from django.contrib import admin
+import logging
+
+from django.contrib import admin, messages
 
 from djapp import models
+from djapp.biz.matching import Matcher
+
+
+logger = logging.getLogger(__name__)
 
 
 @admin.register(models.User)
@@ -22,6 +28,31 @@ class HostOrganizationAdmin(admin.ModelAdmin):
     list_display = ('name', 'contact_email', 'contact_first_name', 'contact_last_name', 'start_date', 'created')
     search_fields = ('name', 'contact_email', 'contact_first_name', 'contact_last_name', 'zip_code')
     ordering = ('-created',)
+    actions = ('run_matchings',)
+
+    def run_matchings(self, request, queryset):
+        n_error = 0
+        n_success = 0
+        matcher = Matcher()
+        for host in queryset:
+            try:
+                matcher.run_process_for_host(host)
+                n_success += 1
+            except Exception as ex:
+                n_error += 1
+                logger.exception('Error processing matchings %r', host)
+                self.message_user(request, (
+                    'Erreur de traitement sur #%s - %s: %s'
+                ) % (host.pk, host.name, ex), messages.ERROR)
+        if n_error == 0:
+            self.message_user(request, (
+                '%s structure(s) traitée(s) avec succès'
+            ) % (n_success,), messages.SUCCESS)
+        else:
+            self.message_user(request, (
+                '%s erreur(s), %s structure(s) traitée(s) avec succès'
+            ) % (n_error, n_success), messages.ERROR)
+    run_matchings.short_description = 'Lancer le matching'
 
 
 @admin.register(models.Matching)
