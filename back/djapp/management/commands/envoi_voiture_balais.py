d45c211d7c18b53677a6f8581c083a7687a5daf7
Loic GUILLOIS <contact@loicguillois.fr>
2021-01-25 17:21:04 +0100

    ajout du script de voiture balais + correctif front

---
+++ back/djapp/management/commands/envoi_voiture_balais.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from djapp import models
+
+from djapp.biz import email_factory
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('--offset', type=int)
+        parser.add_argument('--limit', type=int)
+
+    def handle(self, *args, **options):
+        offset = options['offset']
+        limit = options['limit']
+        for coach in models.Coach.objects.order_by('id').all()[offset:limit]:
+            print("coucou")
+            matchings = models.Matching.objects.all().filter(coach=coach).count()
+            print(matchings)
+            if matchings == 0:
+                email_factory.send_voiture_balais(coach)
+        self.stdout.write('Ok.')
--- front/src/app/candidature/components/page-coach-disponible/page-coach-disponible.component.ts
+++ front/src/app/candidature/components/page-coach-disponible/page-coach-disponible.component.ts
@@ -17,6 +17,8 @@ export class PageCoachDisponibleComponent implements OnInit {
   ladda: boolean;
   key: string;
   disponible: string;
+  showNo: boolean;
+  showYes: boolean;
 
   constructor(
     private api: ApiService,
@@ -42,6 +44,8 @@ export class PageCoachDisponibleComponent implements OnInit {
           res => {
             this.errorMessages = [];
             this.ladda = false;
+            this.showYes = this.disponible === "oui";
+            this.showNo = this.disponible === "non";
             console.log('result', res);
           },
           error => {
