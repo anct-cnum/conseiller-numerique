cf551e2514264b193c4a247fc0ad57cebb670edb
Jean-Yves Stervinou <jy@stervinou.com>
2021-09-16 09:55:23 +0200

    Nouvelle clé de test ReCaptcha pour la recette

--- front/src/environments/environment.uat.ts
+++ front/src/environments/environment.uat.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://uat-cnum-back.osc-fr1.scalingo.io',
   apiSiretUrl: 'https://api.conseiller-numerique.gouv.fr',
-  reCaptachaSiteKey: '6LczlOMZAAAAAP3h0xt-XlfSlqVQczT_nCxPV-dS',
+  reCaptachaSiteKey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
 };
